6410519fda48706b8f7232001a98b2b3a1f8413d
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-04-25 20:49:07 +0700

    feat: enter command

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,21 @@ import Prompt from "./ui/prompt";
 export default function Home() {
   const [path, setPath] = useState("~");
   const [command, setCommand] = useState("");
+  const [history, setHistory] = useState<
+    {
+      path: string;
+      command: string;
+    }[]
+  >([]);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       const regex = /^[a-zA-Z0-9 !@#$%^&*()-_=+[{\]};:'"\<.>/?|\\]+$/;
       const key = e.key;
-      if (key === "Backspace") {
+      if (key === "Enter") {
+        setHistory([...history, { path, command }]);
+        setCommand("");
+      } else if (key === "Backspace") {
         setCommand(`${command.slice(0, -1)}`);
       } else if (regex.test(key) && key.length == 1) {
         setCommand(`${command}${key}`);
@@ -27,12 +36,18 @@ export default function Home() {
   return (
     <>
       <ul className="max-w-full">
+        {history &&
+          history.map((pathPrompt) => (
+            <li>
+              <Prompt
+                userPath={pathPrompt.path}
+                userCommand={pathPrompt.command}
+              />
+            </li>
+          ))}
         <li>
           <Prompt userPath={path} userCommand={command} />
         </li>
-        {/* <li>
-          <Prompt userPath="~/.config/nvim" userCommand="nvim x.html" />
-        </li> */}
       </ul>
     </>
   );
