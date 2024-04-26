baa50913c720f65981f3e155fdb9280fa7d064aa
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-04-26 22:42:56 +0700

    feat: blinking cursor

--- app/ui/prompt.tsx
+++ app/ui/prompt.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Prompt({
   userPath,
   userCommand,
@@ -7,12 +9,22 @@ export default function Prompt({
   userPath: string;
   userCommand: string;
 }) {
+  const [cursor, setCursor] = useState(true);
+
+  useEffect(() => {
+    const cursorInterval = setInterval(() => {
+      setCursor(!cursor);
+    }, 200);
+    return () => clearInterval(cursorInterval);
+  }, [cursor]);
+
   return (
     <span>
       <span className="text-green-500">you@miku4j</span>
       <span>:</span>
       <span className="text-blue-500">{userPath}</span>
       <span>$ {userCommand}</span>
+      <span className={cursor ? "inline" : "hidden"}>_</span>
     </span>
   );
 }
